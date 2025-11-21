0995abdce5024e88baef3866d5f18f2dd98f1779
Vlad Baranov <KrakenField@yandex.ru>
2025-11-21 17:42:25 +0300

    Реализована красивая и корректно работающая талица стоимости услуг

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,3 @@
-// Функция для переключения раскрытия строки
 function toggleRow(element) {
     element.classList.toggle('expanded');
 }
-
-// Обработчики для предотвращения всплытия событий
-//document.addEventListener('DOMContentLoaded', function() {
-//    const noteTriggers = document.querySelectorAll('.note-trigger');
-//    
-//    noteTriggers.forEach(trigger => {
-//        trigger.addEventListener('click', function(e) {
-//            e.stopPropagation(); // Предотвращаем раскрытие строки при клике на примечание
-//        });
-//    });
-//});
