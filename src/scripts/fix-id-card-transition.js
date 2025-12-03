ef21562faf5911a399a093c5b644e270b651fbfa
Vlad Baranov <KrakenField@yandex.ru>
2025-12-03 23:49:45 +0300

    Сайт разделён на страницы. На все страницы добавлен каким-то образом отсутствоваший заголовок html

--- src/scripts/fix-id-card-transition.js
+++ src/scripts/fix-id-card-transition.js
@@ -2,12 +2,13 @@ const active_part_cards = document.querySelectorAll('.active-info-part-card');
 
 let mobile_status = window.innerWidth <= 925;
 
+
+console.log('1');
+
 window.addEventListener('resize', () => {
 
-	const nowMobile = window.innerWidth <= 925;
-
 	if (nowMobile !== mobile_status) {
-
+		console.log('Resize event fired');
 		mobile_status = nowMobile;
 
 		for(let i = 0; i < active_part_cards.length; i++) {
