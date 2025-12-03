f017d4000e6bca48f9eec7d04530a28e47f8cdd3
Vlad Baranov <KrakenField@yandex.ru>
2025-12-03 20:23:24 +0300

    Сделана отдельная цветовая палитра для текста акцентных кнопок. Сделать розовое имя в тёмно теме id -карты. Добавллена ссылка на главную стьраницу в логотип. Добавлена логика сохранения состояния темы в localStorage

--- src/scripts/toggleScheme.js
+++ src/scripts/toggleScheme.js
@@ -1,16 +1,39 @@
 const body = document.querySelector('body');
 
-if ( window.matchMedia("(prefers-color-scheme: dark").matches) {
-	body.classList.toggle('dark-mode');
-}
+let savedDarkTheme = localStorage.getItem('darkTheme');
+let preferDarkTheme = window.matchMedia("(prefers-color-scheme: dark").matches;
 
+
+if (savedDarkTheme === null) {
+	if (preferDarkTheme) {
+		body.classList.add('dark-mode');
+		
+		localStorage.setItem('darkTheme', 'true');
+	} else {
+		localStorage.setItem('darkTheme', 'false');
+	}
+} else {
+	if (savedDarkTheme === 'true') {
+		body.classList.add('dark-mode');
+	}
+}
 
 
 const toggleScheme = document.getElementsByClassName("toggle-scheme");
 
 for (let i = 0; i < toggleScheme.length; i++){
 	toggleScheme[i].addEventListener("click", function(){
-		
+
 		body.classList.toggle('dark-mode');
+
+		savedDarkTheme = localStorage.getItem('darkTheme');
+
+		if (savedDarkTheme === 'true') {
+			localStorage.setItem('darkTheme', 'false');
+		} else {
+			localStorage.setItem('darkTheme', 'true');
+		}
+
+
 	});
 }
